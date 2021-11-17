8a2b0a769661fa7a2f9aebb71782b5ca49b371a7
PressTabs <thomastabsbrooks@gmail.com>
2021-11-16 23:24:43 -0700

    Updated the goals for the data handling portion of the program and it's features

--- load_data.py
+++ load_data.py
@@ -6,6 +6,15 @@ import json
 #   of the start date, due date, and progress to the assignment along with a potential "structure" that imposes how
 #   progress is mapped (i.e instead of 20% across 5 days, it can be 15% for the first 4 days and then 40% for the
 #   final day. A function (or at least a reference to a function) will also be a valid input to the "structure".
+#   The assignment can also have a type that dictates a preset structure without requiring custom values such as
+#   "Book", "Chem Work", "Math Problem Set" etc. Each assignment or assignment type contains a tag indicating
+#   whether it's time data should be tracked. Old data will be recorded and kept but only a set interval of data
+#   defined in the assignment or assignment type will be used to estimate the completion time of an assignment
+#   along with a selection of algorithms that can be used to estimate the time based on the type of data the
+#   assignment represents or the category/division it is in (i.e Chemistry vs Math or Chemistry Labs vs Chemistry
+#   Worksheets).
+#   Notes can also be attached to assignments. The goal of the system is to offer rigidity in scheduling whilst
+#   also aiding the user in completion of the work.
 
 with open("data.json") as file:
 
